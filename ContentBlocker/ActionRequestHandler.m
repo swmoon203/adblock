39517cf4fe9da325c95b328a3d8f4854c065f4fe
Sung Wook Moon <mtjddnr@gmail.com>
2015-09-20 13:36:20 +0900

    whitelist injection

--- ContentBlocker/ActionRequestHandler.m
+++ ContentBlocker/ActionRequestHandler.m
@@ -9,24 +9,66 @@
 #import "ActionRequestHandler.h"
 
 @interface ActionRequestHandler ()
-
+@property (strong, readonly) NSURL *blockerListPath;
+@property (strong, readonly) NSURL *whiteListPath;
+@property (strong, readonly) NSURL *path;
 @end
 
 @implementation ActionRequestHandler
+- (NSURL *)blockerListPath {
+    return [[[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.AdBlock"] URLByAppendingPathComponent:@"blockerList.json"];
+}
+- (NSURL *)whiteListPath {
+    return [[[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.AdBlock"] URLByAppendingPathComponent:@"whiteList.json"];
+}
+- (NSURL *)path {
+    return [[[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.AdBlock"] URLByAppendingPathComponent:@"output.json"];
+}
 
 - (void)beginRequestWithExtensionContext:(NSExtensionContext *)context {
+    [self buildList];
     
-    NSURL *jsonPath = [[[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.AdBlock"] URLByAppendingPathComponent:@"blockerList.json"];
-    
-    if ([[NSFileManager defaultManager] fileExistsAtPath:[jsonPath path]] == NO) {
-        jsonPath = [[NSBundle mainBundle] URLForResource:@"blockerList" withExtension:@"json"];
-    }
-    
-    NSItemProvider *attachment = [[NSItemProvider alloc] initWithContentsOfURL:jsonPath];
+    NSItemProvider *attachment = [[NSItemProvider alloc] initWithContentsOfURL:self.path];
     NSExtensionItem *item = [[NSExtensionItem alloc] init];
     item.attachments = @[attachment];
     
     [context completeRequestReturningItems:@[item] completionHandler:nil];
 }
 
+- (void)buildList {
+    NSURL *jsonPath = self.blockerListPath;
+    if ([[NSFileManager defaultManager] fileExistsAtPath:[jsonPath path]] == NO) {
+        jsonPath = [[NSBundle mainBundle] URLForResource:@"blockerList" withExtension:@"json"];
+    }
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:[self.whiteListPath path]] == NO) {
+        [[NSFileManager defaultManager] copyItemAtURL:jsonPath toURL:self.path error:nil];
+        return;
+    }
+    
+    NSMutableArray *list = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:jsonPath]
+                                                           options:NSJSONReadingMutableContainers
+                                                             error:nil];
+    [self injectWhiteList:list];
+    
+    NSError *err;
+    NSData *output = [NSJSONSerialization dataWithJSONObject:list options:0 error:&err];
+    [output writeToURL:self.path atomically:NO];
+}
+
+- (void)injectWhiteList:(NSMutableArray *)list {
+    NSArray *whiteList = [NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:self.whiteListPath]
+                                                         options:NSJSONReadingMutableContainers
+                                                           error:nil];
+    if ([whiteList count] == 0) return;
+    
+    [list enumerateObjectsUsingBlock:^(NSMutableDictionary *item, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSMutableDictionary *trigger = item[@"trigger"];
+        if (trigger == nil) return;
+        NSMutableArray *unless = trigger[@"unless-domain"];
+        NSMutableSet *set = [NSMutableSet setWithArray:whiteList];
+        if (unless) [set addObjectsFromArray:unless];
+        trigger[@"unless-domain"] = [set allObjects];
+    }];
+}
 @end
